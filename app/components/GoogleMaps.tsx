5a4a8f850f07ca4d0fc7c7f84da6efb8259a7b3e
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2024-01-13 18:17:29 +0000

    upload project

---
+++ app/components/GoogleMaps.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+
+export default function GoogleMaps() {
+	const mapRef = React.useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const initializeMap = async () => {
+			const loader = new Loader({
+				apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+				version: 'quartely',
+			});
+
+			const { Map } = await loader.importLibrary('maps');
+
+			const locationInMap = {
+				lat: 39.60128890889341,
+				lng: -9.069839810859907,
+			};
+
+			// MARKER
+			const { Marker } = (await loader.importLibrary(
+				'marker'
+			)) as google.maps.MarkerLibrary;
+
+			const options: google.maps.MapOptions = {
+				center: locationInMap,
+				zoom: 15,
+				mapId: 'NEXT_MAPS_TUTS',
+			};
+
+			const map = new Map(mapRef.current as HTMLDivElement, options);
+
+			// add the marker in the map
+			const marker = new Marker({
+				map: map,
+				position: locationInMap,
+			});
+		};
+
+		initializeMap();
+	}, []);
+
+	return <div className="h-[600px]" ref={mapRef} />;
+}
